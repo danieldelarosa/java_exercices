2b5d12a57755766a3e67c6ecc2225111689ebf5c
Daniel De la rosa <danndelarosam@gmail.com>
2020-04-05 16:58:06 -0500

    Sedes_Colombia
    Aclaración tema de sedes

--- Array_Elecciones.java
+++ Array_Elecciones.java
@@ -4,7 +4,7 @@ elecciones del rector de la Uniremigton Sede Yopal.
 Los datos deben almacenarse en una matriz donde
 cada fila corresponde a una sede y cada columna corresponde a
 un candidato. Se presentaron 5 candidatos para la eleccion.
-El programa debe mostrar la tabla con los nombres de las sedes;
+El programa debe mostrar la tabla con los nombres de las sedes de Colombia;
 y los nombres de los candidatos y cada uno de los resultados.
 La aplicación también debe mostrar el candidato ganador.
 */
